5c46586e166d495500df3a98e23e38abd4673f0a
Borgitoh <kevenconde1998@gmail.com>
2024-10-16 13:21:52 +0100

    Create modulos

---
+++ src/app/modules/sysconp/components/modal/mudulos-modal/mudulos-modal.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mudulos-modal',
+  templateUrl: './mudulos-modal.component.html',
+  styleUrls: ['./mudulos-modal.component.scss']
+})
+export class MudulosModalComponent {
+
+}
--- src/app/modules/sysconp/sysconp.module.ts
+++ src/app/modules/sysconp/sysconp.module.ts
@@ -5,7 +5,7 @@ import { SysconpRoutingModule } from './sysconp-routing.module';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { RouterModule } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { Briefcase, CalendarDays, CheckCheck, CircleSlash, Clock3, Download, Edit, Ellipsis, Eye, EyeOff, File, FileIcon, FileText, Home, Lock, LucideAngularModule, Menu, Pencil, Plus, ShoppingCart, Trash2, UserCheck, Users } from 'lucide-angular';
+import { Briefcase, CalendarDays, CheckCheck, CircleSlash, Clock3, Download, Edit, Ellipsis, Eye, EyeOff, File, FileIcon, FileText, Home, Lock, LucideAngularModule, Menu, Pencil, Plus, Settings, ShoppingCart, Trash2, UserCheck, Users } from 'lucide-angular';
 import { AgendamentoComponent } from './components/agendamento/agendamento.component';
 import { AgendamentoModalComponent } from './components/modal/agendamento-modal/agendamento-modal.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -18,6 +18,8 @@ import { ProjetosComponent } from './components/projetos/projetos.component';
 import { ProjetoModalComponent } from './components/modal/projeto-modal/projeto-modal.component';
 import { TokenInterceptor } from './token.interceptor';
 import { LoadingInterceptor } from './loading.interceptor';
+import { ModulosComponent } from './components/modulos/modulos.component';
+import { MudulosModalComponent } from './components/modal/mudulos-modal/mudulos-modal.component';
 registerLocaleData(localePt);
 @NgModule({
   declarations: [
@@ -30,13 +32,15 @@ registerLocaleData(localePt);
     UsuarioComponent,
     UsuarioModalComponent,
     ProjetosComponent,
-    ProjetoModalComponent
+    ProjetoModalComponent,
+    ModulosComponent,
+    MudulosModalComponent
   ],
   imports: [
     CommonModule,
     ReactiveFormsModule,
     FormsModule,
-    LucideAngularModule.pick({ Edit, Trash2, Plus, Ellipsis, Home, CalendarDays, Briefcase, FileText, Users, ShoppingCart, Clock3,  Download, CheckCheck, Pencil, CircleSlash, Menu, Lock, EyeOff , Eye}),
+    LucideAngularModule.pick({Settings, Edit, Trash2, Plus, Ellipsis, Home, CalendarDays, Briefcase, FileText, Users, ShoppingCart, Clock3,  Download, CheckCheck, Pencil, CircleSlash, Menu, Lock, EyeOff , Eye}),
     SysconpRoutingModule,
     RouterModule,
     HttpClientModule,
